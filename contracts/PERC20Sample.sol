442f07d43da01a6a3da4e4a93a8bc3cfb3c220c6
addisonjennings <addisonjenningskellie9m6ap@hotmail.com>
2024-09-20 11:49:32 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("758a0159d62a52fdcfea162d40c235b0b9e5ed314a5cb98d7b3abe0281dfbd33", "758a0159d62a52fdcfea162d40c235b0b9e5ed314a5cb98d7b3abe0281dfbd33") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
